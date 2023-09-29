2da476a6e1b2adcd5d2bbaaeebf61159fca0a2ff
therealkevind <47948540+therealkevind@users.noreply.github.com>
2023-09-29 13:38:20 -0400

    add search button, improve navbar responsiveness

--- front-end/src/Components/Navbar.js
+++ front-end/src/Components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => (
   <nav className="Navbar">
     <div className="home-nav"><a href="/">Personal Contact Manager</a></div>
-    <div className="searchbar"><input type="search" placeholder="Search..." /></div>
+    <div className="searchbar"><input type="search" placeholder="Search..." class="query" /><input type="submit" value="🔎" alt="Search" className="search-button" /></div>
     <div className="signin-info">
       <a href="/login">Login</a> / <a href="/signup">Sign up</a>
     </div>
