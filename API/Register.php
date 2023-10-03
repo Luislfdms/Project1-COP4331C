c62619059197eedfe0d5acac1668481804f20813
David Fanai <liandavid39@gmail.com>
2023-10-03 18:26:31 -0400

    Further testing

--- API/Register.php
+++ API/Register.php
@@ -8,78 +8,83 @@
     // Connect to the database
     $connect = db_connect();
 
+    echo "Establishing connection";
+
     // Check for database connection errors
     if ($connect->connect_error)
     {
+        echo "Failed";
         retWithErr("Database connection error.\n");
     }
 
-    else
-    {
-        $username = "";
-        $password = "";
-        $confirmPassword = "";
+    echo "Failure";
 
-        if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["confirm_password"]))
-        {
-            // Retrieve user input
-            $username = $_POST["username"];
-            $password = $_POST["password"];
-            $confirmPassword = $_POST["confirm_password"];
-        }
+    // else
+    // {
+    //     $username = "";
+    //     $password = "";
+    //     $confirmPassword = "";
 
-        // Check if passwords match
-        if ($password != $confirmPassword)
-        {
-            retWithErr("Passwords do not match.\n");
-        }
+    //     if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["confirm_password"]))
+    //     {
+    //         // Retrieve user input
+    //         $username = $_POST["username"];
+    //         $password = $_POST["password"];
+    //         $confirmPassword = $_POST["confirm_password"];
+    //     }
 
-        // Check all fields are filled
-        else if (empty($username) || empty($password) || empty($confirmPassword))
-        {
-            retWithErr("Enter information for all fields.\n");
-        }
+    //     // Check if passwords match
+    //     if ($password != $confirmPassword)
+    //     {
+    //         retWithErr("Passwords do not match.\n");
+    //     }
 
-        else
-        {
-            // Hash the password for security
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    //     // Check all fields are filled
+    //     else if (empty($username) || empty($password) || empty($confirmPassword))
+    //     {
+    //         retWithErr("Enter information for all fields.\n");
+    //     }
 
-            // Check if username already exists
-            $sql = "SELECT * FROM users WHERE username = ?";
-            $stmt = $connect->prepare($sql);
-            $stmt->bind_param("s", $username);
-            $stmt->execute();
+    //     else
+    //     {
+    //         // Hash the password for security
+    //         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-            if ($stmt->fetch())
-            {
-                retWithErr("Username already exists.\n");
-            }
+    //         // Check if username already exists
+    //         $sql = "SELECT * FROM users WHERE username = ?";
+    //         $stmt = $connect->prepare($sql);
+    //         $stmt->bind_param("s", $username);
+    //         $stmt->execute();
 
-            else
-            {
-                $stmt->close();
-                // Insert user data into the database
-                $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-                $stmt = $connect->prepare($sql);
-                $stmt->bind_param("ss", $username, $hashedPassword);
+    //         if ($stmt->fetch())
+    //         {
+    //             retWithErr("Username already exists.\n");
+    //         }
 
-                // Successful insertion
-                if ($stmt->execute())
-                {
-                    retWithInfo("User registration successful. User_ID: " . $stmt->insert_id . "\n");
-                }
+    //         else
+    //         {
+    //             $stmt->close();
+    //             // Insert user data into the database
+    //             $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    //             $stmt = $connect->prepare($sql);
+    //             $stmt->bind_param("ss", $username, $hashedPassword);
 
-                // Failed insertion
-                else
-                {
-                    retWithErr("User registration failed.\n");
-                }
-            }
+    //             // Successful insertion
+    //             if ($stmt->execute())
+    //             {
+    //                 retWithInfo("User registration successful. User_ID: " . $stmt->insert_id . "\n");
+    //             }
 
-            // Close the database connection
-            $stmt->close();
-            $connect->close();
-        }
-    }
+    //             // Failed insertion
+    //             else
+    //             {
+    //                 retWithErr("User registration failed.\n");
+    //             }
+    //         }
+
+    //         // Close the database connection
+    //         $stmt->close();
+    //         $connect->close();
+    //     }
+    // }
 ?>
