b5c5801c9b0c3d920bcd498b06b5ce3c726658e5
David Fanai <liandavid39@gmail.com>
2023-10-03 13:13:14 -0400

    Testing for CORS error

--- API/Register.php
+++ API/Register.php
@@ -2,6 +2,7 @@
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods:  POST, GET');
     header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
     include "Auxillary.php";
 
     // Connect to the database
