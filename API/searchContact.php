d191e103247b79e2fcae0f6ebb67294193c8a5de
David Fanai <liandavid39@gmail.com>
2023-10-07 00:10:57 -0400

    Cleaning up search

--- API/searchContact.php
+++ API/searchContact.php
@@ -41,31 +41,27 @@
         $stmt->execute();
         $result = $stmt->get_result();
 
-        // if ($result->num_rows > 0)
+        // Initialize an array to store contact data
+        $contacts = array();
+
+        // Fetch the contact details and display all of them
+        while ($row = $result->fetch_assoc())
         {
-            // Initialize an array to store contact data
-            $contacts = array();
-
-            // Fetch the contact details and display all of them
-            while ($row = $result->fetch_assoc())
-            {
-                $contact = array(
-                    "user_id"       => $row["user_id"],
-                    "contact_id"    => $row["contact_id"],
-                    "first_name"    => $row["first_name"],
-                    "last_name"     => $row["last_name"],
-                    "email"         => $row["email"],
-                    "phone_number"  => $row["phone_number"],
-                );
-
-                $contacts[] = $contact;
-            }
-
-            // Convert the array to JSON
-            $search = json_encode($contacts);
-            sendResInfoAsJson($search);
+            $contact = array(
+                "user_id"       => $row["user_id"],
+                "contact_id"    => $row["contact_id"],
+                "first_name"    => $row["first_name"],
+                "last_name"     => $row["last_name"],
+                "email"         => $row["email"],
+                "phone_number"  => $row["phone_number"],
+            );
+            $contacts[] = $contact;
         }
-        // Close the database connection
-        $connect->close();
+
+        // Convert the array to JSON
+        $search = json_encode($contacts);
+        sendResInfoAsJson($search);
     }
+    // Close the database connection
+    $connect->close();
 ?>
