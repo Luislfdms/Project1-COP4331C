00d3ac31d876c39720a483dcff1a57a4884c0ee6
David Fanai <liandavid39@gmail.com>
2023-10-07 00:01:47 -0400

    Fixing update

--- API/updateContact.php
+++ API/updateContact.php
@@ -57,9 +57,9 @@
             else
             {
                 // Check if a email or phone number already exist
-                $sql = "SELECT * FROM contacts WHERE (email = ? OR phone_number = ?) AND user_id = ?";
+                $sql = "SELECT * FROM contacts WHERE (email = ? OR phone_number = ?) AND user_id = ? AND contact_id = ?";
                 $stmt = $connect->prepare($sql);
-                $stmt->bind_param("sss", $email, $phone, $user_id);
+                $stmt->bind_param("sss", $email, $phone, $user_id, $contact_id);
                 $stmt->execute();
                 $result = $stmt->get_result();
 
